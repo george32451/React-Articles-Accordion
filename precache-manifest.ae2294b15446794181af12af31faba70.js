8445f511d2192abdcc689eb6a79309f719e5fd7a
gmarkosov <george32451@mail.ru>
2018-10-30 18:15:41 +0300

    Updates

---
+++ precache-manifest.ae2294b15446794181af12af31faba70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d76214bfa39bbca42362",
+    "url": "/React-Articles-Accordion/static/js/runtime~main.d76214bf.js"
+  },
+  {
+    "revision": "f0a463903b605d616e83",
+    "url": "/React-Articles-Accordion/static/js/main.f0a46390.chunk.js"
+  },
+  {
+    "revision": "4b9254595c0e3a0d73b4",
+    "url": "/React-Articles-Accordion/static/js/1.4b925459.chunk.js"
+  },
+  {
+    "revision": "f0a463903b605d616e83",
+    "url": "/React-Articles-Accordion/static/css/main.275c7b42.chunk.css"
+  },
+  {
+    "revision": "4b9254595c0e3a0d73b4",
+    "url": "/React-Articles-Accordion/static/css/1.0e8f2cf4.chunk.css"
+  },
+  {
+    "revision": "8a1ba98fa365bfbec3d51d5a72a27b45",
+    "url": "/React-Articles-Accordion/index.html"
+  }
+];
